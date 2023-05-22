615f88a739e72ef0c0ba4e03aaa01925f322a983
chustert <72422384+chustert@users.noreply.github.com>
2023-05-22 18:37:42 +1200

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ window.addEventListener('DOMContentLoaded', function() {
   // Get the tabs container element by its ID
   var tabsContainer = document.getElementById('capture-features_category-tabs-nav');
   // Get all tab links within the tabs container
-  var tabLinks = tabsContainer.getElementsByClassName('category-tab-link');
+  var tabLinks = tabsContainer.getElementsByClassName('capture-features_category-tab-link');
 
   // Function to invert the images
-  function invertImage() {
+  function captureFeatures_invertImage() {
     // Get all the images within the tabs container
-    var allImages = tabsContainer.querySelectorAll('.category-tabs-nav_image');
+    var allImages = tabsContainer.querySelectorAll('.capture-features_category-tabs-nav_image');
     // Invert all the images initially
     allImages.forEach(function(image) {
       image.style.filter = 'invert(1)';
@@ -17,16 +17,16 @@ window.addEventListener('DOMContentLoaded', function() {
     // Get the clicked tab element
     var clickedTab = this;
     // Get the image associated with the clicked tab
-    var clickedImage = clickedTab.querySelector('.category-tabs-nav_image');
+    var clickedImage = clickedTab.querySelector('.capture-features_category-tabs-nav_image');
     // Reset the filter for the clicked image
     clickedImage.style.filter = 'none';
   }
 
   // Run the initial inversion on page load, using the first tab as the default selected tab
-  invertImage.call(tabLinks[0]);
+  captureFeatures_invertImage.call(tabLinks[0]);
 
   // Add click event listeners to the tab links
   for (var i = 0; i < tabLinks.length; i++) {
-    tabLinks[i].addEventListener('click', invertImage);
+    tabLinks[i].addEventListener('click', captureFeatures_invertImage);
   }
 });
